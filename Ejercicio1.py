a739156aae87cc2f174960ac5cdce31c253b338f
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-11-04 18:22:46 +0100

    Ejercicio 1 no acabado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,17 +1,19 @@
 from ast import main
-class alumno:
-    nombre = "Introduzca el nombre del alumn@: "
-    nota = int(input("Introduzca la nota del alumn@: "))
 
-    def calificacion():
-        if nota >= 5:
+class alumno:
+    nombre = input("Introduzca el nombre del alumn@: ")
+    nota = float(input("Introduzca la nota del alumn@: "))
+    def __init__(self):
+        print("Nombre: ", self.nombre)
+        print("Nota: ", self.nota)
+    
+    def calificacion(self):
+        if self.nota >= 5:
             print("APROBADO")
         else:
             print("SUSPENDIDO")
 
-
-
-
-
-if__name__=="__main__":
+if__name__=='__main__':
     main()
+    print(alumno)
+
