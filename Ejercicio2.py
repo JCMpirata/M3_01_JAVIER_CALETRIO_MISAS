ce310312a942c5ed214b13b84757dd65d6c04a79
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-11-04 20:58:48 +0100

    Ejercicio 2 no terminado

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,32 +1,30 @@
 from ast import main
 
 class alumno:
-    alumno1 = input("Introduzca el nombre del alumn@: ")
-    nota1 = float(input("Introduzca la nota del alumn@: "))
-    alumno2 = input("Introduzca el nombre del alumn@: ")
-    nota2 = float(input("Introduzca la nota del alumn@: "))
-    alumno3 = input("Introduzca el nombre del alumn@: ")
-    nota3 = float(input("Introduzca la nota del alumn@: "))
-
-    def __init__(self):
-        print("Nombre: ", self.alumno1)
-        print("Nota: ", self.nota1)
-        print("Nombre: ", self.alumno2)
-        print("Nota: ", self.nota2)
-        print("Nombre: ", self.alumno3)
-        print("Nota: ", self.nota3)
+    
+    def __init__(self, alumno, nota):
+        self.alumno = alumno
+        self.nota = float(nota)
 
     def calificacion(self):
-        for self.nota in self.nota1, self.nota2, self.nota3:
-            if self.nota >= 5:
+        for self.resultado in self.nota:
+            if self.resultado >= 5:
                 print("APROBADO")
             else:
                 ("SUSPENDIDO")
 
+    def __str__(self):
+        cadena = "El alumno" + self.alumno + "ha sacado" + self.nota
+        return cadena
+
 
 if "__name__"=="__main__":
-    alumno1 = alumno()
-    alumno1.__init__()
-    alumno1.calificacion()
+    alumno1 = alumno("Javier", 6.5)
+    alumno2 = alumno("Pablo", 4.75)
+    alumno3 = alumno("Adrian", 8.2)
+
+    print(alumno1)
+    print(alumno2)
+    print(alumno3)
     main()
     
