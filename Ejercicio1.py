0b02a30a8b6ce847caaec5f9892f3c7a66788089
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-11-04 09:55:39 +0100

    Comienzo

---
+++ Ejercicio1.py
@@ -0,0 +1,17 @@
+from ast import main
+class alumno:
+    nombre = "Introduzca el nombre del alumn@: "
+    nota = int(input("Introduzca la nota del alumn@: "))
+
+    def calificacion():
+        if nota >= 5:
+            print("APROBADO")
+        else:
+            print("SUSPENDIDO")
+
+
+
+
+
+if__name__=="__main__":
+    main()
